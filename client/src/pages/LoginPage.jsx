fc3125785dcbc3d604d02c17a7591a702d358550
StewartSidauruk <220711816@students.uajy.ac.id>
2025-07-16 21:23:13 +0700

    fix:modified login form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import { auth } from '../configs/firebase';
-import { useNavigate } from 'react-router';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from "react";
+import { auth } from "../configs/firebase";
+import { useNavigate } from "react-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   async function handleLogin(e) {
@@ -17,35 +17,57 @@ export default function LoginPage() {
         password
       );
       console.log(userLoggedIn);
-      navigate('/');
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
   }
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleLogin} action="">
-        <div>
-          <label htmlFor="">Email</label>
-          <br />
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="">Password</label>
-          <br />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button>Login</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-xs">
+        <h1>Login Page</h1>
+        <form
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleLogin}
+          action=""
+        >
+          <div>
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Email
+            </label>
+            <br />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div>
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <br />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
